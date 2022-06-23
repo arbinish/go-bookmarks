2574df62f59d52a83ce7dc82536c5549d0157aed
Binish A R <binish@apple.com>
2022-06-23 01:09:38 -0700

    feat: add option to open urls with default browser

--- cli/cmd/list.go
+++ cli/cmd/list.go
@@ -7,6 +7,8 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 
 import (
 	"fmt"
+	"os/exec"
+	"runtime"
 
 	"github.com/arbinish/go-bookmarks/bookmarks"
 	"github.com/spf13/cobra"
@@ -22,6 +24,11 @@ var listCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		name := cmd.Flag("name").Value.String()
 		tag := cmd.Flag("tag").Value.String()
+		open := cmd.Flag("open").Value.String()
+		var urls = []string{}
+		var err error
+		var openCmd string
+
 		client := newClient("http://localhost:4912", 5)
 		if name == "" && tag == "" {
 			fmt.Println(cmd.UsageString())
@@ -41,8 +48,29 @@ var listCmd = &cobra.Command{
 			fmt.Printf("%s: not found\n", param)
 			return
 		}
-		for _, p := range r {
-			fmt.Printf("%s\n", p)
+		for i, p := range r {
+			fmt.Printf("%d| %s\n", i+1, p)
+			urls = append(urls, p.URL)
+		}
+		if open == "false" {
+			return
+		}
+		switch runtime.GOOS {
+		case "darwin":
+			openCmd = "open"
+		case "linux":
+			openCmd = "xdg-open"
+		default:
+			if open != "false" {
+				fmt.Printf("Unsupported OS detected")
+				return
+			}
+		}
+		for _, url := range urls {
+			fmt.Printf("opening %s\n", url)
+			if err = exec.Command(openCmd, url).Run(); err != nil {
+				fmt.Println("failed to open url", url, ":", err)
+			}
 		}
 	},
 }
@@ -59,6 +87,7 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-	listCmd.PersistentFlags().String("name", "", "short name to look up")
-	listCmd.PersistentFlags().String("tag", "", "tag to search for")
+	listCmd.PersistentFlags().String("name", "", "short name to look up.")
+	listCmd.PersistentFlags().String("tag", "", "tag to search for.")
+	listCmd.PersistentFlags().Bool("open", false, "open url in default browser. default: false, do not open url in browser.")
 }
